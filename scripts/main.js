d5e4be901f45fcfbcdab7d1b535c3a7dd9df5560
Amanda Favaro <amandacfa@hotmail.com>
2022-03-22 13:13:02 -0300

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,32 @@
 const email = document.getElementById("email");
 const message = document.getElementById("message");
-const resposta = document.getElementById("resposta");
+const faleConosco = document.getElementById("faleConosco");
+const response = document.createElement("p");
 
 function enviarMensagem(event) {
   event.preventDefault();
+  response.id = "response";
   if (validarMensagem()) {
     if (validarEmail(email.value) && validarEmailComprimento(email)) {
-      resposta.innerHTML =
+      response.innerHTML =
         "Obrigado pelo contato, " +
         email.value.substring(0, email.value.indexOf("@")) +
         "!";
     } else {
-      resposta.innerHTML = "Erro no envio: Endereço de e-mail inválido!";
+      response.innerHTML = "Erro no envio: Endereço de e-mail inválido!";
     }
   } else {
-    resposta.innerHTML = "Erro no envio: Insira uma mensagem!";
+    response.innerHTML = "Erro no envio: Insira uma mensagem!";
   }
+  faleConosco.appendChild(response);
 }
 
 function validarMensagem() {
-  if (message.value) {
-    return true;
-  } else {
-    return false;
-  }
+  return message.value ? true : false;
 }
 
 function validarEmail(email) {
-  const re = /[A-Za-z0-9.]+@[a-z0-9]+\.\S+/;
+  const re = /^[A-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/;
   return re.test(email);
 }
 
@@ -37,14 +36,36 @@ function validarEmailComprimento(email) {
     email.value.indexOf("@") + 1,
     email.value.indexOf(".")
   );
-  if (
-    user.length >= 1 &&
+  return user.length >= 1 &&
     user.length <= 32 &&
     domain.length >= 1 &&
     domain.length <= 16
-  ) {
-    return true;
-  } else {
-    return false;
-  }
+    ? true
+    : false;
 }
+
+// function validarMensagem() {
+//   if (message.value) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// function validarEmailComprimento(email) {
+//   const user = email.value.substring(0, email.value.indexOf("@"));
+//   const domain = email.value.substring(
+//     email.value.indexOf("@") + 1,
+//     email.value.indexOf(".")
+//   );
+//   if (
+//     user.length >= 1 &&
+//     user.length <= 32 &&
+//     domain.length >= 1 &&
+//     domain.length <= 16
+//   ) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
